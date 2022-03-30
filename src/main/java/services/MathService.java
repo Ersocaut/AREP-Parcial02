3beda540731c97a36b35e4a70365f2b732bc2707
Ersocaut <leonardo.soy.yo@hotmail.com>
2022-03-30 13:23:41 -0500

    Spark ON locally

---
+++ src/main/java/services/MathService.java
@@ -0,0 +1,15 @@
+package services;
+
+import java.lang.Math.*;
+
+public class MathService {
+
+    public double getLn(double n){
+        return 0;
+    }
+
+    public double getAsin(double n){
+        return 0;
+    }
+
+}
