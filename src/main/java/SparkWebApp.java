c4a007783b3d6c7ae9c4c6242a14aad7e7342e3b
Ersocaut <leonardo.soy.yo@hotmail.com>
2022-03-30 14:24:47 -0500

    Math services working locally

--- src/main/java/SparkWebApp.java
+++ src/main/java/SparkWebApp.java
@@ -1,9 +1,27 @@
 import static spark.Spark.*;
 
+import services.MathService;
+
 public class SparkWebApp {
     public static void main(String[] args) {
         port(getPort());
         get("/hello", (req, res) -> "Hello world from spark.");
+
+        get("/ln", (req, res) -> {
+            res.type("application/json");
+            double sol = MathService.getLn(Double.parseDouble(req.queryParams("value")));
+            return "{\"operation\":\"ln\"," +
+                    "\"input\": " + Double.parseDouble(req.queryParams("value")) + "," +
+                    "\"output\":" + sol + "}";
+        });
+
+        get("/asin", (req, res) -> {
+            res.type("application/json");
+            double sol = MathService.getAsin(Double.parseDouble(req.queryParams("value")));
+            return "{\"operation\":\"asin\"," +
+                    "\"input\": " + Double.parseDouble(req.queryParams("value")) + "," +
+                    "\"output\":" + sol + "}";
+        });
     }
 
     static int getPort(){
--- src/main/java/services/MathService.java
+++ src/main/java/services/MathService.java
@@ -4,12 +4,12 @@ import java.lang.Math.*;
 
 public class MathService {
 
-    public double getLn(double n){
-        return 0;
+    public static double getLn(double n){
+        return Math.log(n);
     }
 
-    public double getAsin(double n){
-        return 0;
+    public static double getAsin(double n){
+        return Math.asin(n);
     }
 
 }
